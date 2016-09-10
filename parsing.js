ea3401a42044dc73a4bad1f6fb6e43a66fa982b0
name": null <email": null>
2016-09-10 16:49:50 +0700

    scraping hacker ok

--- parsing.js
+++ parsing.js
@@ -1,24 +1,53 @@
-"use strict"
+'use strict'
 
-var fs = require('fs')
-var cheerio = require('cheerio')
-var doc
+const cheerio = require('cheerio')
+const fs = require('fs')
+let request = require('request')
+let user = []
+let story = []
+let comment = []
+let col = ["\x1b[30m",
+    "\x1b[31m",
+    "\x1b[32m",
+    "\x1b[33m",
+    "\x1b[34m",
+    "\x1b[35m",
+    "\x1b[36m",
+    "\x1b[37m"
+  ]
+  // let col = [FgBlack = "\x1b[30m",
+  //   FgRed = "\x1b[31m",
+  //   FgGreen = "\x1b[32m",
+  //   FgYellow = "\x1b[33m",
+  //   FgBlue = "\x1b[34m",
+  //   FgMagenta = "\x1b[35m",
+  //   FgCyan = "\x1b[36m",
+  //   FgWhite = "\x1b[37m"]
 
-fs.readFile('post.html', function (err, data) {
-  if (err) {
-    console.error(err)
-  }
-  doc = cheerio.load(data)
+request({
+  method: 'GET',
+  url: 'https://news.ycombinator.com/newcomments'
+}, function (err, response, body) {
+  if (err) return console.error(err)
 
 
-  extract_usernames(doc)
-
-})
-
-function extract_usernames(doc) {
-  doc('span.comhead > a:first-child').filter(function (i, element) {
-    var data = doc(this)
-    console.log(data[0]['children'][0].data)
+  let $ = cheerio.load(body)
+  $('.hnuser').each(function () {
+    user.push($(this).text())
   })
-}
 
+
+  $('.storyon > a:first-child').each(function () {
+    story.push($(this).text())
+  })
+
+  $('.c00').each(function () {
+    comment.push($(this).text().replace(/(\w{7}).*/g, "$1...."))
+  })
+
+  user.forEach((val, idx) => {
+    console.log(col[2], val, "commented on:", col[4], story[idx])
+    console.log(col[(idx + 1) % 8], "--------------------------------------------------")
+    console.log(col[idx % 8], comment[idx])
+  })
+})
